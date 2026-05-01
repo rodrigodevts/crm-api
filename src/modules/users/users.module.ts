e320f3e7c7b5243557bddc42f65640042d0dbebc
Rodrigo Alves <rodrigo.sousa.devjs@gmail.com>
2026-05-01 19:28:52 -0300

    feat(users): wire AuthModule into UsersModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+// import { MeController } from './controllers/me.controller';  // Task 21
 import { UsersController } from './controllers/users.controller';
 import { UsersApplicationService } from './services/users.application.service';
 import { UsersDomainService } from './services/users.domain.service';
 
 @Module({
-  controllers: [UsersController],
+  imports: [AuthModule],
+  controllers: [UsersController /*, MeController*/],
   providers: [UsersApplicationService, UsersDomainService],
-  exports: [UsersApplicationService],
+  exports: [UsersApplicationService, UsersDomainService],
 })
 export class UsersModule {}
